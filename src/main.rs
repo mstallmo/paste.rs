90c1d28c0faea82e5d76a1091aa7906e0d02a5f4
Mason Stallmo <masonstallmo@gmail.com>
2018-10-09 20:59:19 -0500

    Update dependencies and implement revised public API for igniter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-#![feature(plugin, decl_macro, proc_macro_non_items)]
+#![feature(plugin, decl_macro)]
 #![plugin(rocket_codegen)]
 
 #[macro_use]
 extern crate diesel;
 extern crate rocket;
+extern crate rocket_igniter;
+
+use rocket_igniter::engine::{CliCommand, Engine};
 
 #[cfg(test)]
 mod tests;
@@ -11,14 +14,18 @@ mod tests;
 #[macro_use]
 mod macros;
 mod api;
-mod database;
 mod app;
+mod database;
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/", routes![app::index, app::files, app::catch_unknown_routes])
+        .mount(
+            "/",
+            routes![app::index, app::files, app::catch_unknown_routes],
+        )
         .mount("/api/v1", routes![api::index, api::upload, api::retrieve])
         .manage(database::init_pool())
+        .attach(Engine::new(CliCommand::YARN))
 }
 
 fn main() {
